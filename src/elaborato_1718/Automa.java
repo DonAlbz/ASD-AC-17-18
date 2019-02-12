066d4a10b08dae3b0ec656eb54fcf24d1bb34caf
DonAlbz <alberto.vivenzi@gmail.com>
2019-02-12 14:04:40 +0100

    Scheletro del progetto

--- src/elaborato_1718/Automa.java
+++ src/elaborato_1718/Automa.java
@@ -5,10 +5,28 @@
  */
 package elaborato_1718;
 
+import java.util.Vector;
+
 /**
  *
  * @author Alb
  */
 public class Automa {
-    
+
+    private Vector<Stato> stati;
+    private Stato statoCorrente = stati.firstElement();
+    private String descrizione;
+
+    boolean isAbilitato() {
+        return statoCorrente.isAbilitato();
+    }
+
+    void scatta(Transazione t) {
+        this.statoCorrente = statoCorrente.scatta(t);
+    }
+
+    void scatta() {
+        this.statoCorrente = statoCorrente.scatta();
+    }
+
 }
--- src/elaborato_1718/Transazione.java
+++ src/elaborato_1718/Transazione.java
@@ -5,10 +5,35 @@
  */
 package elaborato_1718;
 
+import java.util.Vector;
+
 /**
  *
  * @author Alb
  */
 public class Transazione {
+
+    private Vector<Evento> linkIn;
+    private Vector<Evento> linkOut;
+    private Stato statoDestinazione;
+    private String descrizione;     
     
+    boolean isAbilitato() {
+        for (Evento link : linkIn) {
+            if (link == null) {
+                return false;
+            }
+        }
+        for (Evento link : linkOut) {
+            if (link != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    Stato scatta() {
+        return statoDestinazione;
+    }
+
 }
