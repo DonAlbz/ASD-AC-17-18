1e0a5b061cf30cd7a3d4d5f4de726198288fe970
DonAlbz <alberto.vivenzi@gmail.com>
2019-02-12 11:12:33 +0100

    Scheletro del progetto

--- src/elaborato_1718/Stato.java
+++ src/elaborato_1718/Stato.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package elaborato_17.pkg18;
+package elaborato_1718;
 
 /**
  *
  * @author Alb
  */
-public class Transazione {
+public class Stato {
     
 }
